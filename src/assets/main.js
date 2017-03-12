9c1b239b09b1549509cffb75136ead7ed126e967
lylemyer <lyleluvzme@yahoo.com>
2017-03-12 04:28:39 -1000

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,1 +1,43 @@
+$(function() {
 
+
+  $.ajax({
+    url: 'https://www.codeschool.com/users/lylemyer.json',
+    dataType: 'jsonp',
+    success: function(response) {
+    addCourses(response.courses.completed);
+    }
+  });
+function addCourses(courses){
+
+var $badges = $('#badges');
+
+  courses.forEach(function(course)  {
+
+//console.log(course);
+
+var $course = $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+    }).appendTo($course);
+
+      $('<img></img>', {
+      src: course.badge
+   }).appendTo($course)
+
+
+
+      $('<a />', {
+    'class': 'btn btn-primary',
+    target: '_blank',
+    href: course.url,
+    text: 'See Course'
+  }).appendTo($course);
+
+  })
+}
+
+});
